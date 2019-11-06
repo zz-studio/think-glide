ab2255f4cfdca71f0d3b32ad1cb11b8f436d0b4a
小波🌾 <xiaobosun@gridinfo.com.cn>
2019-11-06 13:11:31 +0800

    update

---
+++ src/Glide.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * +----------------------------------------------------------------------
+ * | think-glide [thinkphp6]
+ * +----------------------------------------------------------------------
+ *  .--,       .--,             | FILE: Glide.php
+ * ( (  \.---./  ) )            | AUTHOR: byron
+ *  '.__/o   o\__.'             | EMAIL: xiaobo.sun@qq.com
+ *     {=  ^  =}                | QQ: 150093589
+ *     /       \                | DATETIME: 2019/11/6 12:58
+ *    //       \\               |
+ *   //|   .   |\\              |
+ *   "'\       /'"_.-~^`'-.     |
+ *      \  _  /--'         `    |
+ *    ___)( )(___               |-----------------------------------------
+ *   (((__) (__)))              | 高山仰止,景行行止.虽不能至,心向往之。
+ * +----------------------------------------------------------------------
+ * | Copyright (c) 2019 http://www.zzstudio.net All rights reserved.
+ * +----------------------------------------------------------------------
+ */
+declare(strict_types=1);
+
+namespace think;
+
+use League\Glide\Server;
+use League\Glide\ServerFactory;
+use League\Glide\Signatures\SignatureFactory;
+use League\Glide\Urls\UrlBuilderFactory;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class Glide
+{
+
+}
---
+++ src/config.php
@@ -0,0 +1,28 @@
+<?php
+// +----------------------------------------------------------------------
+// | thinkphp6 glide [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016 http://www.zzstudio.net All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: Byron Sampson <xiaobo.sun@qq.com>
+// +----------------------------------------------------------------------
+
+use think\facade\Request;
+
+return [
+    // 路由前缀，匹配到该前缀时中间件开始执行
+    'baseUrl' => '/images',
+    // 缓存文件位置
+    'cache' => app()->getRuntimePath() . DIRECTORY_SEPARATOR . 'glide',
+    // 缓存时间，示例 +2 days, 缓存期间多次请求会自动响应 304
+    'cacheTime' => '+1 day',
+    // 安全签名
+    'signKey' => false,
+    'glide' => [],
+    // 异常处理handler
+    'onException' => function(\Exception $exception, Request $request, $server){
+        throw $exception;
+    },
+];
